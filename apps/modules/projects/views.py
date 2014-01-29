5fad1b534b1fcc69f72833e1e63d091b83c9ebcc
Chi-En Wu <jason2506.mail@gmail.com>
2014-01-29 12:34:36 +0800

    Add view for displaying project

--- apps/modules/projects/views.py
+++ apps/modules/projects/views.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, abort
 
 from . import module
+from ... import models
 
 
 @module.route('/')
 def index():
     return render_template('index.html')
+
+
+@module.route('/<project>')
+def show_project(project):
+    project = models.Project.query.filter_by(short_name=project).first()
+    if project is None:
+        return abort(404)
+
+    return render_template('show_project.html', project=project)
