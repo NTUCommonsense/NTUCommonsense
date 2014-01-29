bbcb90588832280acd7ee5e5ce32478a4c0ff98c
Chi-En Wu <jason2506.mail@gmail.com>
2014-01-29 23:20:50 +0800

    Redirect to index page if the user is authenticated

--- apps/modules/admin/views.py
+++ apps/modules/admin/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import request, url_for, redirect, render_template
-from flask.ext.login import login_user, logout_user, login_required
+from flask.ext.login import current_user, login_user, logout_user, login_required
 
 from . import module
 from ...forms import SigninForm
@@ -15,6 +15,9 @@ def index():
 
 @module.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user is not None and current_user.is_authenticated():
+        return redirect(url_for('admin.index'))
+
     form = SigninForm(request.form)
     if form.validate_on_submit():
         login_user(form.user)
