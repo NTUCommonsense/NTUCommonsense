89fb2cfe8d2a3d56e993face45b21ba8016887b7
Chi-En Wu <jason2506.mail@gmail.com>
2014-02-10 23:09:44 +0800

    Extract initialization functions

--- apps/__init__.py
+++ apps/__init__.py
@@ -14,6 +14,25 @@ def _get_projects():
     return Project.query.with_entities(Project.short_name, Project.name).all()
 
 
+def _init_db(app, create_db):
+    db.app = app
+    db.init_app(app)
+    if create_db:
+        db.create_all()
+
+
+def _init_jinja(app):
+    filters.init_app(app)
+    app.jinja_env.globals['get_projects'] = _get_projects
+
+
+def _init_login(app):
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.user_loader(User.get)
+    login_manager.login_view = "/login"
+
+
 def create_app(name=None, create_db=False):
     if name is None:
         name = __name__
@@ -21,19 +40,9 @@ def create_app(name=None, create_db=False):
     app = Flask(name)
     app.config.from_object('config')
 
-    filters.init_app(app)
-
-    db.app = app
-    db.init_app(app)
-    if create_db:
-        db.create_all()
-
-    app.jinja_env.globals['get_projects'] = _get_projects
-
-    login_manager = LoginManager()
-    login_manager.init_app(app)
-    login_manager.user_loader(User.get)
-    login_manager.login_view = "/login"
+    _init_db(app, create_db)
+    _init_jinja(app)
+    _init_login(app)
 
     app.register_blueprint(module)
     return app
