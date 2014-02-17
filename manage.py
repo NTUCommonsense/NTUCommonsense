342848aec703b66ff65362c863ef8a8d7c4fb8c7
Chi-En Wu <jason2506.mail@gmail.com>
2014-02-18 01:28:26 +0800

    Update management command for creating user

--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 from flask.ext.script import Manager, prompt, prompt_bool, prompt_pass
+from werkzeug.datastructures import MultiDict
 
 from apps import create_app
 from apps.models import db, User
+from apps.forms import SignupForm
 
 manager = Manager(create_app)
 
@@ -25,12 +29,26 @@ def dropdb():
 @manager.command
 def createuser():
     '''Creates a new user for the website.'''
-    name = prompt('Username')
-    pwd = prompt_pass('Password')
+    email = prompt('E-mail')
+    name = prompt('Full Name')
+    is_admin = prompt_bool('Admin')
+    raw_pwd = prompt_pass('Password')
+    confirm_pwd = prompt_pass('Confirm Password')
 
-    user = User(name=name)
-    user.set_pwd(pwd)
-    user.save()
+    user = User()
+    data = MultiDict(dict(email=email, name=name, is_admin=is_admin,
+                          raw_pwd=raw_pwd, confirm_pwd=confirm_pwd))
+    form = SignupForm(data, obj=user, csrf_enabled=False)
+    if form.validate():
+        form.populate_obj(user)
+        user.save()
+
+        print('User was successfully created.')
+    else:
+        for field, errors in form.errors.iteritems():
+            field_text = getattr(form, field).label.text
+            for error in errors:
+                print('[Error] {0}: {1}'.format(field_text, error))
 
 
 if __name__ == '__main__':
